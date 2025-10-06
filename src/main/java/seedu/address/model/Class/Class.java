f7dd89f45effefb657405992af1fd46bc82d47ec
Rachel <rachelwongyx@gmail.com>
2025-10-06 15:17:31 +0800

    Create Class class

---
+++ src/main/java/seedu/address/model/Class/Class.java
@@ -0,0 +1,34 @@
+package seedu.address.model.Class;
+
+import seedu.address.model.person.Address;
+import seedu.address.model.person.Person;
+
+import java.time.DayOfWeek;
+import java.time.Duration;
+import java.time.LocalTime;
+
+public class Class {
+    private String subject;
+    private int level;
+    private DayOfWeek day;
+    private LocalTime startTime;
+    private LocalTime endTime;
+    private float hourlyRate;
+    private Address location;
+    private Duration duration;
+
+    public Class(Person person, String subject, int level, DayOfWeek day, LocalTime startTime, LocalTime endTime,
+                 float hourlyRate) {
+        this.subject = subject;
+        assert level < 6 : "Level must be smaller than 6 but was: " + level;
+        assert level > 0 : "Level must be larger than 0 but was: " + level;
+        this.level = level;
+        this.day = day;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.hourlyRate = hourlyRate;
+        this.location = person.getAddress();
+        this.duration = Duration.between(startTime, endTime);
+    }
+
+}
