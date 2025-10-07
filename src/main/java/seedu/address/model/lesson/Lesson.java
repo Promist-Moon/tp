40838042b5ad0eeb25a5f5be64d0a5ffb26fbca1
Rachel <rachelwongyx@gmail.com>
2025-10-08 03:40:16 +0800

    Add getters to Lesson.java

--- src/main/java/seedu/address/model/lesson/Lesson.java
+++ src/main/java/seedu/address/model/lesson/Lesson.java
@@ -1,34 +1,63 @@
 package seedu.address.model.lesson;
 
-import seedu.address.model.person.Address;
-import seedu.address.model.person.Person;
+import seedu.address.model.person.*;
 
 import java.time.DayOfWeek;
 import java.time.Duration;
 import java.time.LocalTime;
 
 public class Lesson {
-    private String subject;
-    private int level;
+    private Subject subject;
+    private Level level;
     private DayOfWeek day;
     private LocalTime startTime;
     private LocalTime endTime;
-    private float hourlyRate;
-    private Address location;
+    private Rate rate;
+    private Address address;
     private Duration duration;
 
-    public Lesson(Person person, String subject, int level, DayOfWeek day, LocalTime startTime, LocalTime endTime,
-                  float hourlyRate) {
+    public Lesson(Person person, Subject subject, Level level, DayOfWeek day, LocalTime startTime, LocalTime endTime,
+                  Rate rate) {
         this.subject = subject;
-        assert level < 6 : "Level must be smaller than 6 but was: " + level;
-        assert level > 0 : "Level must be larger than 0 but was: " + level;
         this.level = level;
         this.day = day;
         this.startTime = startTime;
         this.endTime = endTime;
-        this.hourlyRate = hourlyRate;
-        this.location = person.getAddress();
+        this.rate = rate;
+        this.address = person.getAddress();
         this.duration = Duration.between(startTime, endTime);
+    }
+
+    public Subject getSubject() {
+        return subject;
+    }
+
+    public Level getLevel() {
+        return level;
+    }
+
+    public DayOfWeek getDay() {
+        return day;
+    }
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+
+    public Rate getRate() {
+        return rate;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public Duration getDuration() {
+        return duration;
     }
 
 }
