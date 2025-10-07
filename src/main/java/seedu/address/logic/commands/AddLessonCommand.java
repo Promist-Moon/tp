5c58a817ccff4cfbedaff0f1951d7f37a4704443
Rachel <rachelwongyx@gmail.com>
2025-10-08 02:44:17 +0800

    Update imports in AddLessonCommand.java

--- src/main/java/seedu/address/logic/commands/AddLessonCommand.java
+++ src/main/java/seedu/address/logic/commands/AddLessonCommand.java
@@ -1,57 +1,66 @@
 package seedu.address.logic.commands;
 
+import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_STUDENT_INDEX;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_SUBJECT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_LEVEL;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_DAY;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_START_TIME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_END_TIME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_RATE;
+
 import seedu.address.commons.util.ToStringBuilder;
 import seedu.address.logic.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.lesson.Lesson;
 
-import static java.util.Objects.requireNonNull;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
-
-public class AddLessonCommand {
+/**
+ * Adds a lesson to the address book.
+ */
+public class AddLessonCommand extends Command {
     public static final String COMMAND_WORD = "add.lesson";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a person to the address book. "
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a lesson to the student. "
             + "Parameters: "
-            + PREFIX_NAME + "NAME "
-            + PREFIX_PHONE + "PHONE "
-            + PREFIX_EMAIL + "EMAIL "
-            + PREFIX_ADDRESS + "ADDRESS "
-            + "[" + PREFIX_TAG + "TAG]...\n"
+            + PREFIX_STUDENT_INDEX + "STUDENT INDEX "
+            + PREFIX_SUBJECT + "SUBJECT "
+            + PREFIX_LEVEL + "LEVEL "
+            + PREFIX_DAY + "DAY "
+            + PREFIX_START_TIME + "START "
+            + PREFIX_END_TIME + "END "
+            + PREFIX_RATE + "RATE "
             + "Example: " + COMMAND_WORD + " "
-            + PREFIX_NAME + "John Doe "
-            + PREFIX_PHONE + "98765432 "
-            + PREFIX_EMAIL + "johnd@example.com "
-            + PREFIX_ADDRESS + "311, Clementi Ave 2, #02-25 "
-            + PREFIX_TAG + "friends "
-            + PREFIX_TAG + "owesMoney";
+            + PREFIX_STUDENT_INDEX + "1 "
+            + PREFIX_SUBJECT + "English "
+            + PREFIX_LEVEL + "2 "
+            + PREFIX_DAY + "Monday "
+            + PREFIX_START_TIME + "10:00 "
+            + PREFIX_END_TIME + "12:00 "
+            + PREFIX_RATE + "80 ";
 
     public static final String MESSAGE_SUCCESS = "New lesson added: %1$s";
+    public static final String MESSAGE_DUPLICATE_LESSON = "This timeslot is already taken";
 
     private final Lesson toAdd;
 
     /**
-     * Creates an AddCommand to add the specified {@code Person}
+     * Creates an AddLessonCommand to add the specified {@code Lesson}
      */
-    public AddCommand(Class class) {
-        requireNonNull(class);
-        toAdd = class;
+    public AddLessonCommand(Lesson lesson) {
+        requireNonNull(lesson);
+        toAdd = lesson;
     }
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
 
-        if (model.hasPerson(toAdd)) {
-            throw new CommandException(MESSAGE_DUPLICATE_PERSON);
+        if (model.hasLesson(toAdd)) {
+            throw new CommandException(MESSAGE_DUPLICATE_LESSON);
         }
 
-        model.addPerson(toAdd);
+        model.addLesson(toAdd);
         return new CommandResult(String.format(MESSAGE_SUCCESS, Messages.format(toAdd)));
     }
 
@@ -62,12 +71,12 @@ public class AddLessonCommand {
         }
 
         // instanceof handles nulls
-        if (!(other instanceof AddCommand)) {
+        if (!(other instanceof AddLessonCommand)) {
             return false;
         }
 
-        AddCommand otherAddCommand = (AddCommand) other;
-        return toAdd.equals(otherAddCommand.toAdd);
+        AddLessonCommand otherAddLessonCommand = (AddLessonCommand) other;
+        return toAdd.equals(otherAddLessonCommand.toAdd);
     }
 
     @Override
