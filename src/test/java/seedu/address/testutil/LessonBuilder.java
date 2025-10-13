3d1d6da8fb6d0969838a5f54271140ffa3504ae4
Rachel <rachelwongyx@gmail.com>
2025-10-13 17:08:54 +0800

    Add getSampleLessons method in SampleDataUtil class

---
+++ src/test/java/seedu/address/testutil/LessonBuilder.java
@@ -0,0 +1,12 @@
+package seedu.address.testutil;
+
+import seedu.address.model.lesson.Day;
+import seedu.address.model.lesson.Lesson;
+import seedu.address.model.lesson.LessonTime;
+import seedu.address.model.lesson.Level;
+import seedu.address.model.lesson.Rate;
+import seedu.address.model.lesson.Subject;
+import seedu.address.model.util.SampleDataUtil;
+
+public class LessonBuilder {
+}
