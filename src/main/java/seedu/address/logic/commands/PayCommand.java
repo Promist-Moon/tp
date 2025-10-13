80d0f20c07ecacc96bd1d5af0796c8c8fd271ff6
Emilia Ayu <emiliaayu944@gmail.com>
2025-10-14 01:37:40 +0800

    Add equals and toString to PayCommand
    Create PayCommandParser

--- src/main/java/seedu/address/logic/commands/PayCommand.java
+++ src/main/java/seedu/address/logic/commands/PayCommand.java
@@ -1,21 +1,20 @@
 package seedu.address.logic.commands;
 
-//import static java.util.Objects.requireNonNull;
-//
-//import java.util.List;
+import static java.util.Objects.requireNonNull;
+
+import java.util.List;
 
 import seedu.address.commons.core.index.Index;
-//import seedu.address.commons.util.ToStringBuilder;
-//import seedu.address.logic.Messages;
-//import seedu.address.logic.commands.exceptions.CommandException;
-//import seedu.address.model.Model;
-//import seedu.address.model.person.Person;
-
+import seedu.address.commons.util.ToStringBuilder;
+import seedu.address.logic.Messages;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.person.Person;
 
 /**
  * Marks all payments associated with the specified person as paid.
  */
-public class PayCommand {
+public class PayCommand extends Command {
 
     public static final String COMMAND_WORD = "pay";
 
@@ -26,24 +25,44 @@ public class PayCommand {
             + "Example: " + COMMAND_WORD + "1";
 
     public static final String MESSAGE_PAYMENT_SUCCESS = "Paid: %1$s";
+
     private final Index targetIndex;
 
     public PayCommand(Index targetIndex) {
         this.targetIndex = targetIndex;
     }
 
-//    @Override
-//    public CommandResult execute(Model model) throws CommandException {
-//        requireNonNull(model);
-//        List<Person> lastShownList = model.getFilteredPersonList();
-//
-//        if (targetIndex.getZeroBased() >= lastShownList.size()) {
-//            throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
-//        }
-//
-//        Person personToMarkPaid = lastShownList.get(targetIndex.getZeroBased());
-//        model.markPersonPaid(personToMark);
-//        return
-//    }
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        List<Person> lastShownList = model.getFilteredPersonList();
 
+        if (targetIndex.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        }
+
+        return new CommandResult(String.format(MESSAGE_PAYMENT_SUCCESS, "placeholder success message"));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof PayCommand)) {
+            return false;
+        }
+
+        PayCommand otherPayCommand = (PayCommand) other;
+        return targetIndex.equals(otherPayCommand.targetIndex);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .add("targetIndex", targetIndex)
+                .toString();
+    }
 }
---
+++ src/main/java/seedu/address/logic/parser/PayCommandParser.java
@@ -0,0 +1,29 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.DeleteCommand;
+import seedu.address.logic.commands.PayCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+/**
+ * Parses input arguments and creates a new PayCommand object
+ */
+public class PayCommandParser implements Parser<PayCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the DeleteCommand
+     * and returns a DeleteCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public PayCommand parse(String args) throws ParseException {
+        try {
+            Index index = ParserUtil.parseIndex(args);
+            return new DeleteCommand(index);
+        } catch (ParseException pe) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE), pe);
+        }
+    }
+}
