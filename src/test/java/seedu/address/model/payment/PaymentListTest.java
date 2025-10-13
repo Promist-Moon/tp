5fe8faa9540e23d2a7f710b5d3ba2ae7899f340c
Emilia Ayu <emiliaayu944@gmail.com>
2025-10-13 19:33:54 +0800

    Add PaymentListTest

---
+++ src/test/java/seedu/address/model/payment/PaymentListTest.java
@@ -0,0 +1,22 @@
+package seedu.address.model.payment;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.testutil.Assert.assertThrows;
+import static seedu.address.testutil.TypicalPayments.JAN_25;
+import static seedu.address.testutil.TypicalPayments.FEB_25;
+import static seedu.address.testutil.TypicalPayments.MAR_25_ALICE;
+import static seedu.address.testutil.TypicalPersons.ALICE;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.model.payment.PaymentList;
+
+/**
+ *
+ */
+public class PaymentListTest {
+
+    private final PaymentList paymentList = new PaymentList();
+
+}
