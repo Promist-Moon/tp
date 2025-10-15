c0f3ba4d48b664724da369df0bfc0022ce178240
Ruileirei <angruiwenray@gmail.com>
2025-10-15 10:43:41 +0800

    Update LessonList

--- src/main/java/seedu/address/model/lesson/LessonList.java
+++ src/main/java/seedu/address/model/lesson/LessonList.java
@@ -80,6 +80,27 @@ public class LessonList {
     }
 
     /**
+     * Returns the total duration of all classes in a month.
+     * Returns the total duration of {@code Lesson} hours in a month.
+     * @param month of the query.
+     * @return the total number of {@code Lesson} hours.
+     */
+    public long getTotalHours(YearMonth month) {
+        long totalHours = 0;
+        for (Lesson l : lessons) {
+            Day day = l.getDay();
+
+            // count number of lessons in a month based on local month
+            int daysInMonth = DateTimeUtil.countDaysOfWeekInMonth(month, day);
+            long hoursPerLesson = l.getDurationLong();
+            long hoursPerMonth = daysInMonth * hoursPerLesson;
+
+            totalHours += hoursPerMonth;
+        }
+        return totalHours;
+    }
+
+    /**
      * Checks if the specified lesson exists in this lesson list.
      *
      * <p>Iterates through all lessons starting from index 1 up to the size of the list,
@@ -100,23 +121,5 @@ public class LessonList {
             }
         }
         return false;
-    }
-
-    /**
-     * Returns the total duration of all classes in a month.
-     */
-    public long getTotalHours(YearMonth month) {
-        long totalHours = 0;
-        for (Lesson l : lessons) {
-            Day day = l.getDay();
-
-            // count number of lessons in a month based on local month
-            int daysInMonth = DateTimeUtil.countDaysOfWeekInMonth(month, day);
-            long hoursPerLesson = l.getDurationLong();
-            long hoursPerMonth = daysInMonth * hoursPerLesson;
-
-            totalHours += hoursPerMonth;
-        }
-        return totalHours;
     }
 }
