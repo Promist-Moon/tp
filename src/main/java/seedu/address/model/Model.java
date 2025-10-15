f9295a8a563157d0cb2cc2f4f021954a397b53b4
Rachel <rachelwongyx@gmail.com>
2025-10-15 19:26:45 +0800

    Add setLessons to resetData method in AddressBook class

--- src/main/java/seedu/address/model/Model.java
+++ src/main/java/seedu/address/model/Model.java
@@ -109,4 +109,8 @@ public interface Model {
      * {@code lesson} must exist in the address book.
      */
     void deleteLesson(Student student, Lesson lesson);
+
+    /**
+     * Adds the
+     */
 }
