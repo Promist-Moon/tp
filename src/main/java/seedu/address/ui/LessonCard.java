d603f4ed595f8d06229c2b708108f146f1031da7
Dickson <dicksonlai66@gmail.com>
2025-10-16 13:34:55 +0800

    Resolve checkstyle issue

--- src/main/java/seedu/address/ui/LessonCard.java
+++ src/main/java/seedu/address/ui/LessonCard.java
@@ -2,29 +2,15 @@ package seedu.address.ui;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
 import seedu.address.model.lesson.Lesson;
-import seedu.address.model.person.Person;
-import seedu.address.model.person.student.Student;
-
-import java.util.Comparator;
-
 /**
- * An UI component that displays information of a {@code Person}.
+ * An UI component that displays information of a {@code Lesson}.
  */
 public class LessonCard extends UiPart<Region> {
 
     private static final String FXML = "LessonListCard.fxml";
-
-    /**
-     * Note: Certain keywords such as "location" and "resources" are reserved keywords in JavaFX.
-     * As a consequence, UI elements' variable names cannot be set to such keywords
-     * or an exception will be thrown by JavaFX during runtime.
-     *
-     * @see <a href="https://github.com/se-edu/addressbook-level4/issues/336">The issue on AddressBook level 4</a>
-     */
 
     public final Lesson lesson;
 
@@ -50,7 +36,7 @@ public class LessonCard extends UiPart<Region> {
     public LessonCard(Lesson lesson) {
         super(FXML);
         this.lesson = lesson;
-//        id.setText(lesson.getLessonIndex());
+        // id.setText(lesson.getLessonIndex());
         subject.setText(lesson.getSubject().toString());
         level.setText(lesson.getLevel().toString());
         day.setText(lesson.getDay().toString());
--- src/main/java/seedu/address/ui/LessonListCard.java
+++ src/main/java/seedu/address/ui/LessonListCard.java
@@ -6,12 +6,19 @@ import javafx.fxml.FXML;
 import javafx.scene.layout.VBox;
 import seedu.address.model.lesson.Lesson;
 
+/**
+ * An UI component that displays a list of lessons
+ */
 public class LessonListCard extends UiPart<VBox> {
     private static final String FXML = "LessonListPanel.fxml";
 
     @FXML
     private VBox lessonList;
 
+    /**
+     * Returns a list of lessons in their individual card
+     * @param lessons
+     */
     public LessonListCard(List<Lesson> lessons) {
         super(FXML);
         for (Lesson lesson : lessons) {
