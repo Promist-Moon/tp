8c3b5fec1ed31ca52067f70377ea6623ff1ee306
Emilia Ayu <emiliaayu944@gmail.com>
2025-10-21 13:55:03 +0800

    Add equals and toString method to ViewCommand

--- src/main/java/seedu/address/logic/commands/ViewCommand.java
+++ src/main/java/seedu/address/logic/commands/ViewCommand.java
@@ -5,6 +5,7 @@ import static java.util.Objects.requireNonNull;
 import java.util.List;
 
 import seedu.address.commons.core.index.Index;
+import seedu.address.commons.util.ToStringBuilder;
 import seedu.address.logic.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
@@ -42,6 +43,28 @@ public class ViewCommand extends Command {
         return new CommandResult(String.format(MESSAGE_VIEW_SUCCESS, Messages.format(studentToViewLessons)));
 
 
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof ViewCommand)) {
+            return false;
+        }
+
+        ViewCommand otherViewCommand = (ViewCommand) other;
+        return targetIndex.equals(otherViewCommand.targetIndex);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .add("targetIndex", targetIndex)
+                .toString();
     }
 
 }
