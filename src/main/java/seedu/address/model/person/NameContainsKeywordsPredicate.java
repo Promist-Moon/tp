ac6289c8f0ac844b7306c77e7da4ee4a92d6bd37
Ruileirei <angruiwenray@gmail.com>
2025-10-21 15:03:47 +0800

    Resolve checkstyle issue

--- src/main/java/seedu/address/model/person/NameContainsKeywordsPredicate.java
+++ src/main/java/seedu/address/model/person/NameContainsKeywordsPredicate.java
@@ -1,13 +1,10 @@
 package seedu.address.model.person;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
 
 import seedu.address.commons.util.StringUtil;
 import seedu.address.commons.util.ToStringBuilder;
-import seedu.address.model.lesson.Lesson;
-import seedu.address.model.lesson.LessonList;
 import seedu.address.model.person.student.Student;
 
 /**
