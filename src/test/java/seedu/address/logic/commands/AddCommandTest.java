5f4ce96629c4c11d8bf6ba8c6104938f2c041298
Rachel <rachelwongyx@gmail.com>
2025-10-23 16:55:02 +0800

    Edit ModelStub in AddCommandTest to override methods of superclass

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -171,10 +171,13 @@ public class AddCommandTest {
             throw new AssertionError("This method should not be called.");
         }
 
+        /*
         @Override
         public boolean hasLessonClash(Lesson lesson) {
             throw new AssertionError("This method should not be called.");
         }
+
+         */
 
         @Override
         public void addLesson(Student student, Lesson lesson) {
@@ -199,6 +202,11 @@ public class AddCommandTest {
 
         @Override
         public SortedList<Lesson> getSortedFilteredLessons() {
+            throw new AssertionError("This method should not be called");
+        }
+
+        @Override
+        public void setLesson(Student student, Lesson lesson, Lesson editedLesson) {
             throw new AssertionError("This method should not be called");
         }
     }
