94fe612a0143bca46557dcfca769f7e8a81e7c3f
Dickson <dicksonlai66@gmail.com>
2025-10-30 19:23:38 +0800

    Resolve merge conflict

--- src/main/java/tutman/tuiniverse/logic/commands/EditLessonCommand.java
+++ src/main/java/tutman/tuiniverse/logic/commands/EditLessonCommand.java
@@ -1,39 +1,38 @@
-package seedu.address.logic.commands;
+package tutman.tuiniverse.logic.commands;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
-import static seedu.address.logic.Messages.MESSAGE_INVALID_DISPLAYED_LESSON_INDEX;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_DAY;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_END_TIME;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_LESSON_INDEX;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_LEVEL;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_RATE;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_START_TIME;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_STUDENT_INDEX;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_SUBJECT;
+import static tutman.tuiniverse.commons.util.CollectionUtil.requireAllNonNull;
+import static tutman.tuiniverse.logic.Messages.MESSAGE_INVALID_DISPLAYED_LESSON_INDEX;
+import static tutman.tuiniverse.logic.parser.CliSyntax.PREFIX_DAY;
+import static tutman.tuiniverse.logic.parser.CliSyntax.PREFIX_END_TIME;
+import static tutman.tuiniverse.logic.parser.CliSyntax.PREFIX_LESSON_INDEX;
+import static tutman.tuiniverse.logic.parser.CliSyntax.PREFIX_LEVEL;
+import static tutman.tuiniverse.logic.parser.CliSyntax.PREFIX_RATE;
+import static tutman.tuiniverse.logic.parser.CliSyntax.PREFIX_START_TIME;
+import static tutman.tuiniverse.logic.parser.CliSyntax.PREFIX_STUDENT_INDEX;
+import static tutman.tuiniverse.logic.parser.CliSyntax.PREFIX_SUBJECT;
 
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.function.Predicate;
 
-import seedu.address.commons.core.index.Index;
-import seedu.address.commons.util.CollectionUtil;
-import seedu.address.commons.util.ToStringBuilder;
-import seedu.address.logic.Messages;
-import seedu.address.logic.commands.exceptions.CommandException;
-import seedu.address.model.Model;
-import seedu.address.model.lesson.Day;
-import seedu.address.model.lesson.Lesson;
-import seedu.address.model.lesson.LessonList;
-import seedu.address.model.lesson.LessonTime;
-import seedu.address.model.lesson.Level;
-import seedu.address.model.lesson.Rate;
-import seedu.address.model.lesson.Subject;
-import seedu.address.model.lesson.exceptions.DuplicateLessonException;
-import seedu.address.model.lesson.exceptions.LessonException;
-import seedu.address.model.person.Person;
-import seedu.address.model.person.student.Student;
+import tutman.tuiniverse.commons.core.index.Index;
+import tutman.tuiniverse.commons.util.CollectionUtil;
+import tutman.tuiniverse.commons.util.ToStringBuilder;
+import tutman.tuiniverse.logic.Messages;
+import tutman.tuiniverse.logic.commands.exceptions.CommandException;
+import tutman.tuiniverse.model.Model;
+import tutman.tuiniverse.model.lesson.Day;
+import tutman.tuiniverse.model.lesson.Lesson;
+import tutman.tuiniverse.model.lesson.LessonList;
+import tutman.tuiniverse.model.lesson.LessonTime;
+import tutman.tuiniverse.model.lesson.Level;
+import tutman.tuiniverse.model.lesson.Rate;
+import tutman.tuiniverse.model.lesson.Subject;
+import tutman.tuiniverse.model.lesson.exceptions.DuplicateLessonException;
+import tutman.tuiniverse.model.lesson.exceptions.LessonException;
+import tutman.tuiniverse.model.student.Student;
 
 /**
  * Edits the details of an existing lesson in the address book.
@@ -61,7 +60,6 @@ public class EditLessonCommand extends Command {
     public static final String MESSAGE_DUPLICATE_LESSON = "This lesson clashes with an existing "
             + "lesson in the address book.";
     public static final String MESSAGE_NOT_EDITED = "At least one field to edit must be provided.";
-
     private final Index studentIndex;
     private final Index lessonIndex;
     private final EditLessonDescriptor editLessonDescriptor;
@@ -82,25 +80,14 @@ public class EditLessonCommand extends Command {
     @Override
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
-        List<Person> lastShownList = model.getFilteredPersonList();
-        // Initialising student
-        Student student;
+        List<Student> lastShownList = model.getFilteredPersonList();
 
         if (studentIndex.getZeroBased() >= lastShownList.size()) {
             throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
         }
 
-        Person person = lastShownList.get(studentIndex.getZeroBased());
-
-        if (person instanceof Student) {
-            student = (Student) person;
-        } else {
-            // might have to change to person is not a student
-            throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
-        }
-
-        // Initialising student's lesson list
-        LessonList studentLessonList = student.getLessonList();
+        Student person = lastShownList.get(studentIndex.getZeroBased());
+        LessonList studentLessonList = person.getLessonList();
 
         if (lessonIndex.getOneBased() > studentLessonList.getSize()) {
             throw new CommandException(MESSAGE_INVALID_DISPLAYED_LESSON_INDEX);
@@ -110,13 +97,13 @@ public class EditLessonCommand extends Command {
         try {
             Lesson lessonToEdit = studentLessonList.getLesson(lessonIndex.getOneBased());
             Lesson editedLesson = createEditedLesson(lessonToEdit, editLessonDescriptor);
-
-            model.setLesson(student, lessonToEdit, editedLesson);
+            editedLesson.addStudent(person);
+            model.setLesson(person, lessonToEdit, editedLesson);
             Predicate<Lesson> belongsToStudent =
-                    lesson -> student.getLessonList().hasLesson(lesson);
+                    lesson -> person.getLessonList().hasLesson(lesson);
             model.updateFilteredLessonList(belongsToStudent);
 
-            return new CommandResult(String.format(MESSAGE_EDIT_LESSON_SUCCESS, Messages.format(student)));
+            return new CommandResult(String.format(MESSAGE_EDIT_LESSON_SUCCESS, Messages.format(person)));
 
         } catch (LessonException e) {
             throw new CommandException(MESSAGE_INVALID_DISPLAYED_LESSON_INDEX);
@@ -135,9 +122,9 @@ public class EditLessonCommand extends Command {
         Level updatedLevel = editLessonDescriptor.getLevel().orElse(lessonToEdit.getLevel());
         Rate updatedRate = editLessonDescriptor.getRate().orElse(lessonToEdit.getRate());
         Subject updatedSubject = editLessonDescriptor.getSubject().orElse(lessonToEdit.getSubject());
-        Student student = lessonToEdit.getStudent();
-        Lesson updatedLesson = new Lesson(updatedSubject, updatedLevel, updatedDay, updatedLessonTime, updatedRate);
-        updatedLesson.addStudent(student);
+        String studentName = editLessonDescriptor.getStudentName().orElse(lessonToEdit.getStudentName());
+        Lesson updatedLesson = new Lesson(updatedSubject, updatedLevel, updatedDay,
+                updatedLessonTime, updatedRate, studentName);
         return updatedLesson;
     }
 
@@ -178,6 +165,7 @@ public class EditLessonCommand extends Command {
         private Level level;
         private Rate rate;
         private Subject subject;
+        private String studentName;
 
         public EditLessonDescriptor() {
         }
@@ -227,6 +215,14 @@ public class EditLessonCommand extends Command {
 
         public Optional<Subject> getSubject() {
             return Optional.ofNullable(subject);
+        }
+
+        public void setStudentName(String studentName) {
+            this.studentName = studentName;
+        }
+
+        public Optional<String> getStudentName() {
+            return Optional.ofNullable(studentName);
         }
 
         @Override
