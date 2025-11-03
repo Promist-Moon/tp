27e17d9c8b1ddf5900f1fab9893a4c2d0ac15230
Rachel <rachelwongyx@gmail.com>
2025-11-03 22:02:47 +0800

    Fix Validation Regex in Address to exclude forward slash

--- src/main/java/tutman/tuiniverse/model/student/Address.java
+++ src/main/java/tutman/tuiniverse/model/student/Address.java
@@ -9,13 +9,16 @@ import static tutman.tuiniverse.commons.util.AppUtil.checkArgument;
  */
 public class Address {
 
-    public static final String MESSAGE_CONSTRAINTS = "Addresses can take any values, and it should not be blank";
+    public static final String MESSAGE_CONSTRAINTS = "Addresses can take any values, and it should not be blank and "
+            + "cannot contain forward slashes.";
 
     /*
      * The first character of the address must not be a whitespace,
      * otherwise " " (a blank string) becomes a valid input.
+     * The address must not contain forward slashes "/".
      */
-    public static final String VALIDATION_REGEX = "[^\\s].*";
+    public static final String VALIDATION_REGEX = "^[^\\\\s/][^/]*$";
+
 
     public final String value;
 
