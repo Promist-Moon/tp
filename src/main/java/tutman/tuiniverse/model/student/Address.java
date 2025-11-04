7f427b3eb47f481defa4c67565a278314687a7e6
Rachel <rachelwongyx@gmail.com>
2025-11-04 13:04:11 +0800

    Fix message constrains for address

--- src/main/java/tutman/tuiniverse/model/student/Address.java
+++ src/main/java/tutman/tuiniverse/model/student/Address.java
@@ -9,8 +9,7 @@ import static tutman.tuiniverse.commons.util.AppUtil.checkArgument;
  */
 public class Address {
 
-    public static final String MESSAGE_CONSTRAINTS = "Addresses can take any values, and it should not be blank and "
-            + "cannot contain forward slashes.";
+    public static final String MESSAGE_CONSTRAINTS = "Addresses can take any values, and it should not be blank.";
 
     /*
      * The first character of the address must not be a whitespace,
